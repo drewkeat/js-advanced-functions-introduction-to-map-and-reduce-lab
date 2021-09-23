0b4e83eb38e521a3217cacbc172a3cc238ed3626
Drew Keat <thedrewkeat@gmail.com>
2021-09-23 15:27:07 -0600

    commit tree 29535f2e4667f126d40b9fcc41e03ebb673db1d7

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+
+function mapToNegativize(array) {
+    let result = []
+    array.forEach(ele => result.push(ele * -1))
+    return result
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    let result = []
+    array.forEach(ele => result.push(ele * 2))
+    return result
+}
+
+function mapToSquare(array) {
+    let result = []
+    array.forEach(ele => result.push(ele ** 2))
+    return result
+}
+
+function reduceToTotal(array, start = 0) {
+    array.forEach(ele => start += ele)
+    return start
+}
+
+function reduceToAllTrue(array) {
+    let result = true
+    array.forEach(ele => {
+        if (!!ele === false) {
+            result = false
+        }
+    })
+    return result
+}
+
+function reduceToAnyTrue(array) {
+    let result = false
+    array.forEach(ele => {
+        if (!!ele) {
+            result = true
+        }
+    })
+    return result
+}
